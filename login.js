7c97b7fea7239ac26b0f2ac01c71d8f156342922
Nguyễn Gia Thịnh <93753773+thinhpleiku2021@users.noreply.github.com>
2021-12-27 09:12:00 +0700

    rege

---
+++ login.js
@@ -0,0 +1,52 @@
+//LOGIN CHECK
+var userName = document.querySelector("#name");
+var password = document.querySelector("#password");
+var loginBtn = document.querySelector("#login-btn");
+var loginRight = document.querySelector(".login-right");
+var incorrectAlert = document.querySelector(".incorrect-alert");
+
+loginBtn.addEventListener("click", checkUser);
+
+function checkUser(e) {
+    var login = 0;
+    if (userName.value == "" || password.value == "") {
+        incorrectAlert.innerHTML = `<strong>All fields has not filled. </strong>Please try again!`;
+        incorrectAlert.classList.add("show");
+        setTimeout(function() {
+            incorrectAlert.classList.remove("show");
+        }, 5000);
+    } else {
+        fetch("https://fake-api-sem.herokuapp.com/users")
+            .then((respone) => respone.json())
+            .then((data) => {
+                // console.log(data);
+                data.forEach(function(user) {
+                    // console.log(userName.value);
+                    // console.log(password.value);
+                    if (userName.value == user.user && password.value == user.password) {
+                        // console.log("Thanh cong");
+                        login = 1;
+                        return login;
+                    }
+                });
+                // console.log(login);
+                if (login) {
+                    loginRight.innerHTML = `
+          <div class="success-img text-center"><img src="./images/login_signup/success.png" alt=""></div>
+          <div class="alert alert-dismissible alert-success">
+            <strong>Successfully Login!</strong><a href="index.html" class="alert-link"> back to home page!</a>
+          </div>
+          `;
+                } else {
+                    userName.value = "";
+                    password.value = "";
+                    incorrectAlert.classList.add("show");
+                    setTimeout(function() {
+                        incorrectAlert.classList.remove("show");
+                    }, 5000);
+                }
+            });
+    }
+
+    e.preventDefault();
+}
